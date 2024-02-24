550cb61affdeac44cf642a63928f7df774360e0d
989onan <onanchew@gmail.com>
2024-02-24 00:29:13 -0500

    - Added velocity value as a Dynamic Impulse With Data<Float3> - Added gravity change as a Dynamic Impulse With Data<Float3> - Velocity changes are now calculated first before the deny is calculated. Now the MaxSpeedChange setting is an absolute of X meters per second you can be traveling in terms of velocity before it denies it, greatly improving the consistency of the setting between different velocity change sources.

--- NoKnockBack/NoKnockBack.cs
+++ NoKnockBack/NoKnockBack.cs
@@ -11,6 +11,7 @@ using ProtoFlux.Runtimes.Execution;
 using ProtoFlux.Runtimes.Execution.Nodes.Actions;
 using static FrooxEngine.SessionControlDialog;
 using System.Runtime.Remoting.Contexts;
+using ProtoFlux.Core;
 
 namespace NoKnockBack
 {
@@ -27,7 +28,7 @@ namespace NoKnockBack
         public override string Author => "989onan";
         public override string Link => "https://github.com/989onan/NoKnockBack";
         public override string Name => "NoKnockBack";
-        public override string Version => "1.0.1";
+        public override string Version => "1.1.0";
 
         public override void OnEngineInit()
         {
@@ -44,12 +45,28 @@ namespace NoKnockBack
             [HarmonyPatch(typeof(ApplyCharacterForce), "Do", typeof(FrooxEngineContext))]
             private static bool knockbackPatch(ref bool __result, ApplyCharacterForce __instance, FrooxEngineContext context)
             {
-
-
-                if (__instance.Force.Evaluate(context).Magnitude > Config.GetValue(maxSpeed) && __instance.Character.Evaluate(context).IsUnderLocalUser)
+                CharacterController characterController = __instance.Character.Evaluate(context);
+                if (characterController == null)
                 {
-                    
+                    return false;
+                }
+
+                bool num = __instance.IgnoreMass.Evaluate(context, defaultValue: false);
+                float3 v = __instance.Force.Evaluate(context);
+                float3 v2 = v * context.World.Time.Delta;
+                if (!num)
+                {
+                    v2 /= characterController.ActualMass;
+                }
+
+                v = characterController.LinearVelocity;
+                float3 finalvelocity = v + v2;
+
+                if (finalvelocity.Magnitude > Config.GetValue(maxSpeed) && __instance.Character.Evaluate(context).IsUnderLocalUser)
+                {
+
                     DynamicImpulseHelper.Singleton.TriggerDynamicImpulse(context.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, context);
+                    DynamicImpulseHelper.Singleton.TriggerDynamicImpulseWithArgument<float3>(context.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, finalvelocity, context);
                     __result = __instance.Character.Evaluate(context) != null;
                     Msg("prevented knockback");
                     return false;
@@ -65,10 +82,27 @@ namespace NoKnockBack
             [HarmonyPatch(typeof(ApplyCharacterImpulse), "Do", typeof(FrooxEngineContext))]
             private static bool knockbackPatch2(ref bool __result, ApplyCharacterImpulse __instance, FrooxEngineContext context)
             {
-                if (__instance.Impulse.Evaluate(context).Magnitude > Config.GetValue(maxSpeed) && __instance.Character.Evaluate(context).IsUnderLocalUser)
+                CharacterController characterController = __instance.Character.Evaluate(context);
+                if (characterController == null)
+                {
+                    return false;
+                }
+
+                bool num = __instance.IgnoreMass.Evaluate(context, defaultValue: false);
+                float3 v = __instance.Impulse.Evaluate(context);
+                if (!num)
+                {
+                    v /= characterController.ActualMass;
+                }
+
+                float3 a = characterController.LinearVelocity;
+                float3 finalvelocity = a + v;
+                
+                if (finalvelocity.Magnitude > Config.GetValue(maxSpeed) && __instance.Character.Evaluate(context).IsUnderLocalUser)
                 {
                     
                     DynamicImpulseHelper.Singleton.TriggerDynamicImpulse(context.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, context);
+                    DynamicImpulseHelper.Singleton.TriggerDynamicImpulseWithArgument<float3>(context.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, finalvelocity, context);
                     __result = __instance.Character.Evaluate(context) != null;
                     Msg("prevented knockback");
                     return false;
@@ -77,35 +111,164 @@ namespace NoKnockBack
                 Msg("Finished code for knockback, not powerful enough.");
                 return true;
             }
-            [HarmonyPrefix]
+
+
+            [HarmonyReversePatch]
+            [HarmonyPatch(typeof(CharacterForceField), "GetForce", typeof(CharacterController))]
+            public static float3 GetForce(CharacterForceField __instance, CharacterController characterController)
+            {
+                throw new NotImplementedException("Stub GetForce");
+            }
+
+            [HarmonyReversePatch]
             [HarmonyPatch(typeof(CharacterForceField), "ShouldApplyForce", typeof(CharacterController))]
-            private static bool ShouldApplyForcePatch(ref bool __result, CharacterForceField __instance, CharacterController character)
+            public static bool ShouldApplyForce(CharacterForceField __instance, CharacterController characterController)
+            {
+                throw new NotImplementedException("Stub GetForce");
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(CharacterForceField), "OnContactStart", typeof(ICollider), typeof(ICollider))]
+            private static bool OnContactStartPatch(CharacterForceField __instance, ICollider collider, ICollider other)
+            {
+                float3 finalvelocity = float3.Zero;
+                CharacterController characterController = other.ColliderOwner as CharacterController;
+                if (characterController != null && ShouldApplyForce(__instance, characterController))
+                {
+                    float3 a = GetForce(__instance, characterController);
+                    switch (__instance.ForceMode.Value)
+                    {
+                        case CharacterForceField.Mode.Impulse:
+                            {
+                                float3 a2 = characterController.LinearVelocity;
+                                finalvelocity = a2 + a;
+                                break;
+                            }
+                        case CharacterForceField.Mode.SetVelocity:
+                            finalvelocity = a;
+                            break;
+                        case CharacterForceField.Mode.SetComponent:
+                            {
+                                float3 a2 = characterController.LinearVelocity;
+                                float3 b = a.Normalized;
+                                float3 b2 = MathX.Reject(in a2, in b);
+                                finalvelocity = a + b2;
+                                break;
+                            }
+                    }
+                    if (finalvelocity.Magnitude > Config.GetValue(maxSpeed) && characterController.IsUnderLocalUser)
+                    {
+
+                        DynamicImpulseHelper.Singleton.TriggerDynamicImpulse(__instance.World.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, null);
+                        DynamicImpulseHelper.Singleton.TriggerDynamicImpulseWithArgument<float3>(__instance.World.LocalUser.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, finalvelocity, null);
+                        Msg("prevented knockback");
+                        return false;
+
+                    }
+                    Msg("Finished code for knockback, not powerful enough.");
+                    return true;
+                }
+                Msg("Finished code for knockback, not powerful enough.");
+                return false;
+
+
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(CharacterForceField), "OnContactStay", typeof(ICollider), typeof(ICollider))]
+            private static bool OnContactStayPatch(CharacterForceField __instance, ICollider collider, ICollider other)
             {
 
+                CharacterController characterController = other.ColliderOwner as CharacterController;
+                if (characterController == null || !ShouldApplyForce(__instance, characterController))
+                {
+                    return false;
+                }
 
-                if (__instance.Force.Value.Magnitude > Config.GetValue(maxSpeed) && character.IsUnderLocalUser)
+                float3 a = GetForce(__instance, characterController);
+                float3 a2 = characterController.LinearVelocity;
+                switch (__instance.ForceMode.Value)
+                {
+                    case CharacterForceField.Mode.ConstantForce:
+                        {
+                            float3 b = float3.Zero;
+                            if (a == b)
+                            {
+                                return false;
+                            }
+
+                            float3 b2 = a.Normalized;
+                            
+                            float num = MathX.Dot(in a2, in b2);
+                            float num2 = (characterController.Jump ? __instance.HoldJumpMaxForceVelocity.Value : __instance.MaxForceVelocity.Value);
+                            if (num < num2)
+                            {
+                                float3 b3 = MathX.Reject(in a2, in b2);
+                                float3 a3 = MathX.Project(in a2, in b2);
+                                b = a * __instance.Time.Delta;
+                                a3 += b;
+                                float magnitude = a3.Magnitude;
+                                if (magnitude > num2)
+                                {
+                                    a3 *= num2 / magnitude;
+                                }
+
+                                a2 = (a2 = a3 + b3);
+                            }
+
+                            break;
+                        }
+                    case CharacterForceField.Mode.ConstantVelocity:
+                        a2 = a;
+                        break;
+                }
+
+                if (__instance.MinCharacterVelocity.Value > 0f || __instance.MaxCharacterVelocity.Value < float.MaxValue)
+                {
+                    float3 b = a2;
+                    a2 = MathX.ClampMagnitude(in b, __instance.MaxCharacterVelocity, __instance.MinCharacterVelocity);
+                }
+
+                if (__instance.CharacterVelocityDampeningSpeed.Value > 0f)
+                {
+                    float3 b = a2;
+                    float3 to = float3.Zero;
+                    a2 = MathX.Lerp(in b, in to, __instance.Time.Delta * (float)__instance.CharacterVelocityDampeningSpeed);
+                }
+                
+                float3 finalvelocity = a2;
+                if (finalvelocity.Magnitude > Config.GetValue(maxSpeed) && characterController.IsUnderLocalUser)
                 {
 
                     DynamicImpulseHelper.Singleton.TriggerDynamicImpulse(__instance.World.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, null);
-                    __result = false;
+                    DynamicImpulseHelper.Singleton.TriggerDynamicImpulseWithArgument<float3>(__instance.World.LocalUser.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, finalvelocity, null);
                     Msg("prevented knockback");
                     return false;
 
                 }
                 Msg("Finished code for knockback, not powerful enough.");
                 return true;
+
+
             }
 
             [HarmonyPrefix]
             [HarmonyPatch(typeof(SetCharacterVelocity), "Do", typeof(FrooxEngineContext))]
             private static bool knockbackPatch4(ref bool __result, SetCharacterVelocity __instance, FrooxEngineContext context)
             {
+                CharacterController characterController = __instance.Character.Evaluate(context);
+                if (characterController == null)
+                {
+                    return false;
+                }
 
+                float3 finalvelocity = (__instance.Velocity.Evaluate(context));
 
-                if (__instance.Velocity.Evaluate(context).Magnitude > Config.GetValue(maxSpeed) && __instance.Character.Evaluate(context).IsUnderLocalUser)
+                if (finalvelocity.Magnitude > Config.GetValue(maxSpeed) && __instance.Character.Evaluate(context).IsUnderLocalUser)
                 {
 
                     DynamicImpulseHelper.Singleton.TriggerDynamicImpulse(context.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, context);
+                    DynamicImpulseHelper.Singleton.TriggerDynamicImpulseWithArgument<float3>(context.LocalUser.GetBodyNodeSlot(BodyNode.Root), "knockbackcanceled", true, finalvelocity, context);
                     __result = __instance.Character.Evaluate(context) != null;
                     Msg("prevented knockback");
                     return false;
@@ -121,11 +284,11 @@ namespace NoKnockBack
             private static bool knockbackPatch5(ref bool __result, SetCharacterGravity __instance, FrooxEngineContext context)
             {
 
-
                 if (!Config.GetValue(allowGravityChange) && __instance.Character.Evaluate(context).IsUnderLocalUser)
                 {
 
                     DynamicImpulseHelper.Singleton.TriggerDynamicImpulse(context.LocalUser.GetBodyNodeSlot(BodyNode.Root), "gravitychangecanceled", true, context);
+                    DynamicImpulseHelper.Singleton.TriggerDynamicImpulseWithArgument<float3>(context.LocalUser.GetBodyNodeSlot(BodyNode.Root), "gravitychangecanceled", true, __instance.Gravity.Evaluate(context), context);
                     __result = __instance.Character.Evaluate(context) != null;
                     Msg("prevented gravity change");
                     return false;
